bb31d3bb6ecd4594158988324c779ae8ff4d33a7
Ribbit <echo07200503@gmail.com>
2024-05-20 09:44:58 +0900

    🚀 initial phase

--- src/bin/plugin/index.ts
+++ src/bin/plugin/index.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import merge from './commands/merge.js';
 
 program.name('kplugin').description('kintone plugin development tool');
+
+merge();
 
 program.parse(process.argv);
